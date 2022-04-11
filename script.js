a48d3c52e9fa4daec329b69da1d5604739894e3a
Raphacsilva <raphaelcavalcantesilva1999@gmail.com>
2022-04-10 22:09:49 -0300

    funcionandocomreplay

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
 // PROMPT //
-let quantas = prompt("quantas tu quer (numero par entre 4 e 14 pfvr)");
-quantas = parseInt(quantas, 10);
-
-while (isNaN(quantas)) {
-  alert("isso não é numero");
+let quantas;
+function inicio(){
   quantas = prompt("quantas tu quer (numero par entre 4 e 14 pfvr)");
   quantas = parseInt(quantas, 10);
-}
 
-if (quantas < 3 || quantas > 14 || quantas % 2 !== 0) {
-  while (true) {
-    alert("perai malandro tem que ser par entre 4 e 14");
+  while (isNaN(quantas)) {
+    alert("isso não é numero");
     quantas = prompt("quantas tu quer (numero par entre 4 e 14 pfvr)");
     quantas = parseInt(quantas, 10);
-    if (isNaN(quantas)) {
-      alert("isso ai não é nem numero");
-    }
-    if (quantas === 4) {
-      break;
-    }
-    if (quantas === 6) {
-      break;
-    }
-    if (quantas === 8) {
-      break;
-    }
-    if (quantas === 10) {
-      break;
-    }
-    if (quantas === 12) {
-      break;
-    }
-    if (quantas === 14) {
-      break;
+  }
+
+  if (quantas < 3 || quantas > 14 || quantas % 2 !== 0) {
+    while (true) {
+      alert("perai malandro tem que ser par entre 4 e 14");
+      quantas = prompt("quantas tu quer (numero par entre 4 e 14 pfvr)");
+      quantas = parseInt(quantas, 10);
+      if (isNaN(quantas)) {
+        alert("isso ai não é nem numero");
+      }
+      if (quantas === 4) {
+        break;
+      }
+      if (quantas === 6) {
+        break;
+      }
+      if (quantas === 8) {
+        break;
+      }
+      if (quantas === 10) {
+        break;
+      }
+      if (quantas === 12) {
+        break;
+      }
+      if (quantas === 14) {
+        break;
+      }
     }
   }
 }
-
+inicio();
 // PROMPT //
 // ARRUMAR OS CARDS \/ \/ //
 
@@ -51,40 +54,46 @@ const imgsTotal = [
   "unicornparrot.gif"
 ];
 const imgsuteis = [];
-for (let i = 0; i < quantas / 2; i++) {
-  imgsuteis[i] = imgsTotal[i];
-}
 const imgsuteisreal = [];
-for (let i = 0; i < quantas; i++) {
-  if (i % 2 === 0) {
-    imgsuteisreal[i] = imgsuteis[i / 2];
-  } else {
-    imgsuteisreal[i] = imgsuteis[parseInt(i / 2, 10)];
-  }
-}
-
-imgsuteisreal.sort(comparador);
-function comparador() {
-  return Math.random() - 0.5;
-}
-
 let cardHTML = "";
 
-imgsuteisreal.forEach((img) => {
-  cardHTML += `<div class="card" data-card="${img}">
-    <div class="verso">
-      <img src="images/${img}">
-    </div>
-    <div class="frente">  
-      <img class="frente" src="images/front.png"/>
-    </div>  
-  </div>`;
-});
+function embaralhador(){
+  cardHTML = "";
+  for (let i = 0; i < quantas / 2; i++) {
+    imgsuteis[i] = imgsTotal[i];
+  }
+  
+  for (let i = 0; i < quantas; i++) {
+    if (i % 2 === 0) {
+      imgsuteisreal[i] = imgsuteis[i / 2];
+    } else {
+      imgsuteisreal[i] = imgsuteis[parseInt(i / 2, 10)];
+    }
+  }
 
-container.innerHTML = cardHTML;
+  imgsuteisreal.sort(comparador);
+  function comparador() {
+    return Math.random() - 0.5;
+  }
+
+  imgsuteisreal.forEach((img) => {
+    cardHTML += `<div class="card" data-card="${img}">
+      <div class="verso">
+        <img src="images/${img}">
+      </div>
+      <div class="frente">  
+        <img class="frente" src="images/front.png"/>
+      </div>  
+    </div>`;
+  });
+  container.innerHTML = cardHTML;
+}
+
+embaralhador();
+
 //ARRUMAR OS CARDS  /\ /\ //
 // game \/ \/ //
-const cards = document.querySelectorAll(".card");
+let cards = document.querySelectorAll(".card");
 
 function unlocked() {
   cards.forEach((card) => card.addEventListener("click", flipCard));
@@ -92,6 +101,11 @@ function unlocked() {
 function locked(){
   cards.forEach((card) => card.removeEventListener("click", flipCard));
 }
+function certaslocked(){
+  let cardscertas = document.querySelectorAll(".certa");
+  cardscertas.forEach((card) => card.removeEventListener("click", flipCard));
+}
+
 unlocked();
 
 let contador = 0;
@@ -115,8 +129,6 @@ function flipCard() {
     clicadas++;
     segunda.removeEventListener("click", flipCard);
     contador = 0;
-    console.log(clicadas);
-    console.log(pontos);
   }
   checar();
 }
@@ -126,14 +138,14 @@ function checar() {
     locked();
     if (primeira.dataset.card === segunda.dataset.card) {
       unlocked();
-      console.log(primeira);
-      console.log(segunda);
-      primeira.removeEventListener("click", flipCard);
-      segunda.removeEventListener("click", flipCard);
+      
+      primeira.classList.add("certa");
+      segunda.classList.add("certa");
+      certaslocked();
       primeira = undefined;
       segunda = undefined;
       pontos+=2;
-      win();
+      setTimeout(win,1000);
     } else {
       reflip();
     }
@@ -152,6 +164,7 @@ function reflip() {
     primeira = undefined;
     segunda = undefined;
     unlocked();
+    certaslocked();
   }, 1000);
 }
 
@@ -163,6 +176,7 @@ function win(){
     while(true){
       if(resposta === "sim"){
         replay();
+        
         break;
       }
       if(resposta === "não"){
@@ -172,11 +186,14 @@ function win(){
     }
   }
 }
+
 function replay(){
+  quantas = 0;
+  container.innerHTML = "";
+  inicio();
+  embaralhador();
+  cards = document.querySelectorAll(".card");
   unlocked();
-  cards.forEach((card) => card.classList.remove("flip"));
-  cards.forEach((card) => card.children[1].classList.remove("none"));
-  cards.forEach((card) => card.children[0].classList.add("none"));
   contador = 0;
   clicadas = 0;
   pontos = 0;
